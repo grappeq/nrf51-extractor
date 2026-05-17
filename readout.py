a378c566445f329a0c81d55b5e5b7383f50294cf
Kacper Grabowski <kacper@grabow.ski>
2026-05-17 23:54:54 +0200

    update to work with python 3, comment out failing swd instructions

--- readout.py
+++ readout.py
@@ -1,33 +1,40 @@
-import telnetlib
+import socket
 import re
 import struct
 
-# Based on https://www.pentestpartners.com/security-blog/nrf51822-code-readout-protection-bypass-a-how-to/
+HOST = "127.0.0.1"
+PORT = 4444
 
-HOST="127.0.0.1"
-PORT="4444"
+sock = socket.create_connection((HOST, PORT))
 
-tn = telnetlib.Telnet(HOST, PORT)
-tn.set_debuglevel(0)
-tn.read_until(">")
+def read_until(prompt=b">"):
+    data = b""
+    while prompt not in data:
+        chunk = sock.recv(4096)
+        if not chunk:
+            raise ConnectionError("Socket closed")
+        data += chunk
+    return data
+
+read_until()
 
 def tncmd(cmd):
-    tn.write(cmd + "\n")
-    return tn.read_until(">")
+    sock.sendall((cmd + "\n").encode("ascii"))
+    return read_until().decode("ascii")
 
 tncmd("reset halt")
 
-# Note - the following pc and r4 instructions will need to change based on what
-# you learn by rnning the commands in gdb.config on your particular device that you
-# are reverse engineering.
-with open("dump.bin", "w") as outfile:
-    for addr in xrange(0, int("0x40000", 16), 4):
+with open("dump.bin", "wb") as outfile:
+    for addr in range(0, 0x40000, 4):
         tncmd("reg pc 0x6d4")
-        tncmd("reg r4 " + hex(addr))
+        tncmd(f"reg r4 {hex(addr)}")
         tncmd("step")
+
         resp = tncmd("reg r4")
-        t = re.findall(r'0x[0-9a-fA-F]+', resp)
+        t = re.findall(r"0x[0-9a-fA-F]+", resp)
+
         if t:
             outfile.write(struct.pack("I", int(t[0], 16)))
-        if (addr % int("0x400", 16)) == 0:
-            print hex(addr)
+
+        if (addr % 0x400) == 0:
+            print(hex(addr))
